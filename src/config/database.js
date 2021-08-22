c1b450c4e4f5f22a513a7a28a036a073bef1dd24
Fabian-0 <fabianjuarezgaliote@gmail.com>
2021-08-21 22:59:09 -0600

    fix db url

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 sequelize.authenticate()
   .then(res => console.log('Connection has been established successfully.'))
