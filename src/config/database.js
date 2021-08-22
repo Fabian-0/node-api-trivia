b5fcf08c3d9aa00a25a7c903d200ff2a202828d4
Fabian-0 <fabianjuarezgaliote@gmail.com>
2021-08-21 22:47:05 -0600

    preparing to production

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,6 @@
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME_DEV,
-  process.env.DB_USERNAME_DEV,
-  process.env.DB_PASSWORD_DEV,
-  {
-    host: process.env.DB_HOST_DEV,
-    dialect: "postgres"
-  }
-);
+const sequelize = new Sequelize(DATABASE_URL);
 
 sequelize.authenticate()
   .then(res => console.log('Connection has been established successfully.'))
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const app = require("./api/server");
 const cors = require("cors");
 const { json } = require("express");
